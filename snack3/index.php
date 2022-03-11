b5b138fd423a378107ed406179cf5c761c06fc88
Simone Giusti <info@simonegiusti.it>
2022-03-11 12:58:19 +0100

    push fine mattinata dopo snacks

---
+++ snack3/index.php
@@ -0,0 +1,56 @@
+<?php
+
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+
+    foreach ($posts as $key => $post) {
+
+        echo "<h1>Post del $key </h1>";
+
+        foreach ($post as $singlePost) {
+            echo '<ul style="margin-bottom: 20px">';
+            echo "<li>Titolo:".  $singlePost['title'] . "</li>";
+            echo "<li>Autore:".  $singlePost['author'] . "</li>";
+            echo "<li>Testo:".  $singlePost['text'] . "</li>";
+            echo '</ul>';
+        } 
+    }
+    
+?>
